bdb11149ad44491ad2bed78e862a922e84f52c93
Omar-Atriano <omar.atrianova@udlap.mx>
2023-02-05 17:27:24 -0600

    Clases

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,27 @@ namespace mashUp
 {
     public partial class Form1 : Form
     {
+        static Figure f;
+        Point ptX, ptY, mouse;
+        Bitmap bmpX, bmpY;
+        Graphics gX, gY;
+        bool IsMouseDownX = false;
+        bool IsMouseDownY = false;
+        Canvas canvas;
+        float deltaX = 0;
+        float deltaY = 1;
+        Scene scene;
+        bool isMouseDown = false;
+
+        const int WM_KEYUP = 0x0101;
+        const int WM_KEYDOWN = 0x0100;
         public Form1()
         {
             InitializeComponent();
         }
+
+
+
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
---
+++ Scene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mashUp
+{
+    public class Scene
+    {
+        public List<Figure> Figures;
+
+        public Scene()
+        {
+            Figures = new List<Figure>();
+        }
+    }
+}
