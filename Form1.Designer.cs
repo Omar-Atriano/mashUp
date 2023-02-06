4da1d7d66ac37d4ca5d2977b5745c2dc7a831f13
Omar-Atriano <omar.atrianova@udlap.mx>
2023-02-05 19:28:05 -0600

    form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.PCT_CANVAS = new System.Windows.Forms.PictureBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.PANEL_LEFT = new System.Windows.Forms.Panel();
@@ -38,6 +39,7 @@
             this.sliderX = new System.Windows.Forms.PictureBox();
             this.panelR = new System.Windows.Forms.Panel();
             this.sliderY = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.PANEL_LEFT.SuspendLayout();
@@ -55,6 +57,11 @@
             this.PCT_CANVAS.Size = new System.Drawing.Size(1533, 652);
             this.PCT_CANVAS.TabIndex = 0;
             this.PCT_CANVAS.TabStop = false;
+            this.PCT_CANVAS.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PCT_CANVAS_MouseClick);
+            this.PCT_CANVAS.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.PCT_CANVAS_MouseDoubleClick);
+            this.PCT_CANVAS.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PCT_CANVAS_MouseDown);
+            this.PCT_CANVAS.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PCT_CANVAS_MouseMove);
+            this.PCT_CANVAS.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PCT_CANVAS_MouseUp);
             // 
             // trackBar1
             // 
@@ -85,10 +92,13 @@
             // 
             // treeView1
             // 
+            this.treeView1.BackColor = System.Drawing.Color.Gray;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
             this.treeView1.Size = new System.Drawing.Size(192, 474);
             this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.treeView1_KeyPress);
             // 
             // ADD
             // 
@@ -98,6 +108,7 @@
             this.ADD.TabIndex = 1;
             this.ADD.Text = "button1";
             this.ADD.UseVisualStyleBackColor = true;
+            this.ADD.Click += new System.EventHandler(this.ADD_Click);
             // 
             // panelDown
             // 
@@ -116,6 +127,9 @@
             this.sliderX.Size = new System.Drawing.Size(1492, 18);
             this.sliderX.TabIndex = 0;
             this.sliderX.TabStop = false;
+            this.sliderX.MouseDown += new System.Windows.Forms.MouseEventHandler(this.sliderX_MouseDown);
+            this.sliderX.MouseMove += new System.Windows.Forms.MouseEventHandler(this.sliderX_MouseMove);
+            this.sliderX.MouseUp += new System.Windows.Forms.MouseEventHandler(this.sliderX_MouseUp);
             // 
             // panelR
             // 
@@ -133,19 +147,31 @@
             this.sliderY.Size = new System.Drawing.Size(20, 704);
             this.sliderY.TabIndex = 0;
             this.sliderY.TabStop = false;
+            this.sliderY.MouseDown += new System.Windows.Forms.MouseEventHandler(this.sliderY_MouseDown);
+            this.sliderY.MouseMove += new System.Windows.Forms.MouseEventHandler(this.sliderY_MouseMove);
+            this.sliderY.MouseUp += new System.Windows.Forms.MouseEventHandler(this.sliderY_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1952, 871);
             this.Controls.Add(this.panelR);
             this.Controls.Add(this.panelDown);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.PANEL_LEFT);
             this.Controls.Add(this.PCT_CANVAS);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.PANEL_LEFT.ResumeLayout(false);
@@ -171,6 +197,7 @@
         private System.Windows.Forms.PictureBox sliderX;
         private System.Windows.Forms.Panel panelR;
         private System.Windows.Forms.PictureBox sliderY;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -25,18 +25,213 @@ namespace mashUp
         bool isMouseDown = false;
 
         const int WM_KEYUP = 0x0101;
+
+        private void sliderY_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsMouseDownY = false;
+            gY.Clear(Color.Transparent);
+            gY.DrawLine(Pens.DimGray, bmpY.Width / 2, 0, bmpY.Width / 2, bmpY.Height);
+            gY.FillEllipse(Brushes.Aquamarine, bmpY.Width / 4, bmpY.Height / 2, bmpX.Height / 2, bmpX.Height / 2);
+
+            sliderY.Invalidate();
+        }
+
+        private void sliderY_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseDownY)
+            {
+                gY.Clear(Color.Transparent);
+                gY.DrawLine(Pens.DimGray, bmpY.Width / 2, 0, bmpY.Width / 2, bmpY.Height);
+                gY.FillEllipse(Brushes.Aquamarine, bmpY.Width / 4, e.Y, bmpX.Height / 2, bmpX.Height / 2);
+
+                sliderY.Invalidate();
+                deltaY += (float)(ptY.Y - e.Location.Y) / 500;//------------------
+                ptY.Y = e.Location.Y;
+            }
+        }
+
+        private void sliderY_MouseDown(object sender, MouseEventArgs e)
+        {
+            ptY = e.Location;
+            IsMouseDownY = true;
+        }
+
+        private void sliderX_MouseDown(object sender, MouseEventArgs e)
+        {
+            ptX = e.Location;
+            IsMouseDownX = true;
+        }
+
+        private void sliderX_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseDownX)
+            {
+                gX.Clear(Color.Transparent);
+                gX.DrawLine(Pens.DimGray, 0, sliderX.Height / 2, sliderX.Width, sliderX.Height / 2);
+                gX.FillEllipse(Brushes.Aquamarine, e.X, sliderX.Height / 4, sliderX.Height / 2, sliderX.Height / 2);
+
+                sliderX.Invalidate();
+                deltaX += (float)(e.Location.X - ptX.X) / 3;//------------------
+                ptX.X = e.Location.X;
+            }
+        }
+
+        private void sliderX_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsMouseDownX = false;
+            gX.Clear(Color.Transparent);
+            gX.DrawLine(Pens.DimGray, 0, sliderX.Height / 2, sliderX.Width, sliderX.Height / 2);
+            gX.FillEllipse(Brushes.Aquamarine, sliderX.Width / 2, sliderX.Height / 4, sliderX.Height / 2, sliderX.Height / 2);
+
+            sliderX.Invalidate();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            canvas = new Canvas(PCT_CANVAS);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (f != null && (IsMouseDownX || IsMouseDownY))
+            {
+                f.TranslateToOrigin();
+                f.Scale(deltaY);
+                f.Rotate(deltaX);
+                f.TranslatePoints(f.Centroid);
+            }
+            deltaX = 0;
+            deltaY = 1;
+            canvas.Render(scene);
+        }
+
+        private void PCT_CANVAS_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                f.UpdateAttributes();
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            f = new Figure();
+            scene.Figures.Add(f);
+            TreeNode node = new TreeNode("Fig" + (treeView1.Nodes.Count + 1));
+            node.Tag = f;
+            treeView1.Nodes.Add(node);
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            f = (Figure)treeView1.SelectedNode.Tag;
+            ADD.Select();
+        }
+
+        public static bool IsControlDown()
+        {
+            return (Control.ModifierKeys & Keys.Control) == Keys.Control;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (f == null)
+                return false;
+
+            switch (keyData)
+            {
+                case Keys.Left:
+                    f.Centroid.X -= 3;
+                    break;
+                case Keys.Right:
+                    f.Centroid.X += 3;
+                    break;
+                case Keys.Up:
+                    f.Centroid.Y += -3;
+                    break;
+                case Keys.Down:
+                    f.Centroid.Y += 3;
+                    break;
+                case Keys.Space:
+                    break;
+            }
+            PCT_CANVAS.Select();
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void PCT_CANVAS_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = e.Location;
+            isMouseDown = true;
+        }
+
+        private void PCT_CANVAS_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            PCT_CANVAS.Select();
+        }
+
+        private void PCT_CANVAS_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                mouse.X -= e.X;
+                mouse.Y -= e.Y;
+                f.TranslatePoints(new Point(-mouse.X, -mouse.Y));
+                mouse = e.Location;
+            }
+        }
+
+        private void PCT_CANVAS_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (f != null)
+            {
+                canvas.DrawPixel(e.X, e.Y, Color.White);
+                f.Add(new PointF(e.X, e.Y));
+            }
+        }
+
+        private void treeView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            return;
+        }
+
         const int WM_KEYDOWN = 0x0100;
         public Form1()
         {
             InitializeComponent();
+            Init();
+            IsMouseDownX = false;
         }
 
+        private void Init()
+        {
+            bmpX = new Bitmap(sliderX.Width, sliderX.Height);
+            bmpY = new Bitmap(sliderY.Width, sliderY.Height);
 
+            gX = Graphics.FromImage(bmpX);
+            gY = Graphics.FromImage(bmpY);
+
+            sliderX.Image = bmpX;
+            sliderY.Image = bmpY;
+
+            gX.DrawLine(Pens.DimGray, 0, bmpX.Height / 2, bmpX.Width, bmpX.Height / 2);
+            gX.FillEllipse(Brushes.Aquamarine, bmpX.Width / 2, bmpX.Height / 4, bmpX.Height / 2, bmpX.Height / 2);
+
+            gY.DrawLine(Pens.DimGray, bmpY.Width / 2, 0, bmpY.Width / 2, bmpY.Height);
+            gY.FillEllipse(Brushes.Aquamarine, bmpY.Width / 4, bmpY.Height / 2, bmpX.Height / 2, bmpX.Height / 2);
+
+            scene = new Scene();
+            Figure fig = new Figure();
+            fig.Add(new PointF(100, 120));
+            fig.Add(new PointF(1400, 120));
+            scene.Figures.Add(fig);
+
+        }
 
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-
+            //LBL_STATUS.Text = " ::: " + (float)trackBar1.Value / 100;
+            f.Follow(scene.Figures[0].Pts[0], scene.Figures[0].Pts[1], (float)trackBar1.Value / 100);
         }
     }
 }
